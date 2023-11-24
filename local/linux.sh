526a32766547c0b9059e91cbba595fabf15764f2
Trevor <79394682+trevor256@users.noreply.github.com>
2023-11-24 09:30:55 -0500

    Update linux.sh

--- local/linux.sh
+++ local/linux.sh
@@ -2,119 +2,115 @@
 # Author   : github.com/trevor256
 # Summary  : install and configure applications for a desktop, server.
 # Supported: Debian
+#
+# fgqargRGQARGAERGARGARGFARGFASRGARGARSGAERGQAEWRGWQERGWERGWE
+#
+    CLI_APPS="default-jdk default-jre nodejs npm transmission-cli tree rsync ripgrep fzf curl ffmpeg  shellcheck \
+              ufw fail2ban rkhunter lynis libpam-tmpdir needrestart nzbget ca-certificates curl gnupg"
+    DESKTOP_APPS="krita inkscape blender kdenlive obs-studio audacity chromium nmap tshark maven gradle"
+    NPM_APPS="nodemon bash-language-server"
 
-    CLI_APPS="default-jdk default-jre maven gradle nodejs npm transmission-cli tree rsync \
-              ufw fail2ban rkhunter lynis libpam-tmpdir apt-listbugs needrestart nzbget \
-              ripgrep fzf curl ffmpeg nmap tshark shellcheck ca-certificates curl gnupg"
-
-    GUI_APPS="krita inkscape blender kdenlive obs-studio audacity chromium"
-
-    NPM_APPS="nodemon bash-language-server react jest"
-
-_setup() {
-    echo "wireshark-common wireshark-common/install-setuid boolean false" | debconf-set-selections
-    apt update && apt install -y $CLI_APPS
-}
 _desktop() {
-    apt install -y $GUI_APPS && npm install -g $NPM_APPS
-    wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-    dpkg -i discord.deb
-    su - trevor -c "discord"
-
-    wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.1.0.18144.tar.gz  -O jetbrains-toolbox-2.1.0.18144.tar.gz
+    echo "wireshark-common wireshark-common/install-setuid boolean false" | debconf-set-selections
+    apt update && apt install -y $CLI_APPS $DESKTOP_APPS && npm install -g $NPM_APPS
+    wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" &
+    wget -O jetbrains-toolbox-2.1.0.18144.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.1.0.18144.tar.gz &
+    wget -O steam.deb https://cdn.akamai.steamstatic.com/client/installer/steam.deb &
+    wget -O 4kvideodownloaderplus_1.2.4-1_amd64.deb https://dl.4kdownload.com/app/4kvideodownloaderplus_1.2.4-1_amd64.deb?source=website &
+    wget -O atlauncher.deb https://atlauncher.com/download/deb &
+    wait
     tar -xzf jetbrains-toolbox-2.1.0.18144.tar.gz
-    ./jetbrains-toolbox-2.1.0.18144/jetbrains-toolbox
-
-    wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb -O steam.deb
-    dpkg -i steam.deb
-    su - trevor -c "steam"
-
-    wget https://dl.4kdownload.com/app/4kvideodownloaderplus_1.2.4-1_amd64.deb?source=website -O 4kvideodownloaderplus_1.2.4-1_amd64.deb
-    dpkg -i 4kvideodownloaderplus_1.2.4-1_amd64.deb
-    su - trevor -c "4kvideodownloaderplus"
-
-    wget https://atlauncher.com/download/deb -O atlauncher.deb
-    dpkg -i atlauncher.deb
-    su - trevor -c "atlauncher"
+    dpkg -i discord.deb steam.deb 4kvideodownloaderplus_1.2.4-1_amd64.deb atlauncher.deb
+    su - trevor -c "discord; steam; 4kvideodownloaderplus; atlauncher; ./jetbrains-toolbox-2.1.0.18144/jetbrains-toolbox"
 }
 
 _server(){
-    #MAKE CRON JOBS FOR BACK UP AND COMPRESSION
-    # check boot times with: systemd-analyze blame set grub timeout to 0 saves 10sec on boot
-    systemctl disable NetworkManager-wait-online.service # saves 6 seconds on boot
-    grep -rl GRUB_TIMEOUT=5 /etc/default/grub | xargs sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' && update-grub2
+    sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+    apt update && apt install -y $CLI_APPS
+    curl -OJ $ICON_URL #must be 64x64 server-icon.png
+    #0 3 * * 5 /path/to/your/maintancescript.sh
+    systemctl disable NetworkManager-wait-online.service #-6 seconds #bootsystemd-analyze blame
+    grep -rl GRUB_TIMEOUT=5 /etc/default/grub | xargs sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' && update-grub2 #-10 seconds 
 
-    SSH_CONFIG_FILE="/etc/ssh/sshd_config"
-    if grep -q "^PermitRootLogin" $SSH_CONFIG_FILE; then
-        sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' $SSH_CONFIG_FILE
-    else
-        echo "PermitRootLogin no" >> $SSH_CONFIG_FILE
-    fi
-
+    
+    jelly_dirs="/jelly/downloads /jelly/movies /jelly/shows /jelly/music /jelly/other /jelly/x /jelly/configs"
+    storage_dirs="/storage"
     [ -d "/jelly" ] || mkdir /jelly
-    mkdir /jelly /storage
-    mkdir /jelly/downloads /storage/config /jelly/movies /jelly/shows
-    echo "UUID=9067c3d0-babc-4ffa-b8b9-4212a4fe4cea /jelly ext4 defaults 0 0" >> /etc/fstab
-    echo "UUID=16cc2161-d654-4cf4-a954-a7d61892d08c /storage ext4 defaults 0 0" >> /etc/fstab
+    [ -d "/storage" ] || mkdir /storage
+    mkdir -p $jelly_dirs $storage_dirs
+    add_to_fstab() {
+      local uuid=$1
+      local mount_point=$2
+      local fs_type=$3
+      local options=$4
+      local dump_freq=$5
+      local pass_num=$6
+      if ! grep -qs "$uuid" /etc/fstab; then
+        echo "UUID=$uuid $mount_point $fs_type $options $dump_freq $pass_num" >> /etc/fstab
+      else
+        echo "UUID entry for $uuid already exists in /etc/fstab."
+      fi
+    }
+    add_to_fstab "9067c3d0-babc-4ffa-b8b9-4212a4fe4cea" "/jelly" "ext4" "defaults" "0" "0"
+    add_to_fstab "16cc2161-d654-4cf4-a954-a7d61892d08c" "/storage" "ext4" "defaults" "0" "0"
     systemctl daemon-reload && mount --all
 
+    
     apt install -y nvidia-cuda-toolkit nvidia-driver nvidia-container-toolkit
     curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
     && curl -s -L https://nvidia.github.io/libnvidia-container/debian11/libnvidia-container.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-
     apt update
     nvidia-ctk runtime configure --runtime=docker
     systemctl restart docker
 
-# Ensure the script is run as root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
 
-# Update and Install Dependencies
-apt update
-apt -y install build-essential pkg-config checkinstall git libfaac-dev libgpac-dev ladspa-sdk-dev libunistring-dev libbz2-dev libjack-jackd2-dev libmp3lame-dev libsdl2-dev libopencore-amrnb-dev libopencore-amrwb-dev libvpx-dev libx264-dev libx265-dev libxvidcore-dev libopenal-dev libopus-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev texi2html yasm zlib1g-dev build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+    apt update
+    apt -y install build-essential pkg-config checkinstall git libfaac-dev libgpac-dev ladspa-sdk-dev libunistring-dev libbz2-dev libjack-jackd2-dev libmp3lame-dev libsdl2-dev libopencore-amrnb-dev libopencore-amrwb-dev libvpx-dev libx264-dev libx265-dev libxvidcore-dev libopenal-dev libopus-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev texi2html yasm zlib1g-dev build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+    mkdir -p ~/nvidia/ && cd ~/nvidia/
+    git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+    cd nv-codec-headers && make install
+    cd ~/nvidia/
+    git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
+    cd ~/nvidia/ffmpeg/
+    ./configure --pkg-config-flags="--static" --enable-nonfree --enable-gpl --enable-version3 \
+    --enable-libmp3lame --enable-libvpx --enable-libopus \
+    --enable-opencl --enable-libxcb \
+    --enable-opengl --enable-nvenc --enable-vaapi \
+    --enable-vdpau --enable-ffplay --enable-ffprobe \
+    --enable-libxvid \
+    --enable-libx264 --enable-libx265 --enable-openal \
+    --enable-cuda-nvcc --enable-cuvid --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+    make -j "$(nproc)"
+    echo "export PATH=\$PATH:/root/nvidia/ffmpeg" >> ~/.bashrc
+    source ~/.bashrc
+    
+    MOD_URL="https://cdn.modrinth.com/data/gvQqBUqZ/versions/2KMrj5c1/lithium-fabric-mc1.20-0.11.2.jar https://cdn.modrinth.com/data/P7dR8mSH/versions/n2c5lxAo/fabric-api-0.83.0%2B1.20.jar"
+    wget -O  fabric.jar https://meta.fabricmc.net/v2/versions/loader/1.20.2/0.14.24/0.11.2/server/jar &
+    wget -O /mods $MOD_URL &
+    echo 'eula=true' >> eula.txt 
+    echo "java -Xmx4G -jar https://meta.fabricmc.net/v2/versions/loader/1.20.2/0.14.24/0.11.2/server/jar nogui" > run.sh 
+    printf "[Unit]
+    Description=run.sh on startup
+    Wants=network.target
+    After=network.target
+    
+    [Service]
+    Nice=5
+    KillMode=control-group
+    SuccessExitStatus=0 1
+    ReadWriteDirectories=/home/admin/
+    WorkingDirectory=/home/admin/
+    ExecStart=/home/admin/run.sh
+    
+    [Install]
+    WantedBy=multi-user.target" > /etc/systemd/system/minecraft.service 
+    wait
+    systemctl enable minecraft.service
+    systemctl start minecraft.service
 
-mkdir -p ~/nvidia/ && cd ~/nvidia/
-git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
-cd nv-codec-headers && make install
-cd ~/nvidia/
-git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
-cd ~/nvidia/ffmpeg/
-
-# Configure FFmpeg with NVIDIA Support
-./configure --pkg-config-flags="--static" --enable-nonfree --enable-gpl --enable-version3 \
---enable-libmp3lame --enable-libvpx --enable-libopus \
---enable-opencl --enable-libxcb \
---enable-opengl --enable-nvenc --enable-vaapi \
---enable-vdpau --enable-ffplay --enable-ffprobe \
---enable-libxvid \
---enable-libx264 --enable-libx265 --enable-openal \
---enable-cuda-nvcc --enable-cuvid --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
-
-# Compile FFmpeg
-make -j "$(nproc)"
-
-# Final Steps and Verification
-if [ -f "ffmpeg" ]; then
-    echo "FFmpeg compilation successful. Installing..."
-    checkinstall --pkgname=ffmpeg-nvidia --pkgversion="1:$(date +%Y%m%d%H%M)-git" --backup=no \
-    --deldoc=yes --fstrans=no --default
-    echo "FFmpeg with NVIDIA support installed."
-else
-    echo "FFmpeg compilation failed."
-    exit 1
-fi
-
-# Add FFmpeg to Path
-echo "export PATH=\$PATH:/root/nvidia/ffmpeg" >> ~/.bashrc
-source ~/.bashrc
-
-    #sudo ufw enable
-    #config ufw
+    
 }
 _git() {
     read -r -p "Enter Git user.name: " git_name
@@ -192,11 +188,10 @@ main() {
     case $action in
         debian_desktop|dd)
                 if uname -a | grep "Debian"; then
-                   time _setup
-                   time _desktop
                    time _git
                    time _docker
                    time _aws_gcloud
+                   time _desktop
 # rsync or backup and neovim setup ffmpeg compression
                     echo "done"
 
@@ -208,10 +203,9 @@ main() {
             ;;
         debian_server|ds)
                if uname -a | grep "Debian"; then
-                    time _setup
-                    time _server
                     time _git
                     time _docker
+                    time _server
                     echo "done"
 
                 else
